12dfc9fb873c91db2c677f41bfd90ebc1e99b8c8
dinhchien1504 <91814060+dinhchien1504@users.noreply.github.com>
2024-03-12 11:20:31 +0700

    xong footer

---
+++ main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <div id="header">
+        <?php
+        echo'day la header';
+        include 'header.php';
+        ?>
+        </div>
+    <div id="content">
+        <?php
+        echo 'day la content';
+        ?>
+    </div>
+    <div id ="footer">
+        <?php
+        echo 'day la footer';
+        include 'footer.php';
+        ?>
+    </div>
+</body>
+</html>
