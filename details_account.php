b5895125cea76cbe2c5cd7b64c0086a2cdd33c42
dinhchien1504 <91814060+dinhchien1504@users.noreply.github.com>
2024-03-21 11:42:11 +0700

    fix_footer and header

---
+++ details_account.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <div id="header">
+        <?php
+        include 'header.php';
+        ?>
+        </div>
+    <div id="content">
+        <?php
+        include 'account.php';
+
+?>
+    </div>
+    <div id ="footer">
+        <?php
+        include 'footer.php';
+        ?>
+    </div>
+</body>
+</html>
