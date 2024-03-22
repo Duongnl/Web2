2ab2c5c1f1143f87f96cf499caeb268aa1d48a31
LocHoang1 <lochoang2101@gmail.com>
2024-03-22 21:32:51 +0700

    loc push

---
+++ cart_detail.php
@@ -0,0 +1,52 @@
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="cart_detail.css">
+<div class="cart_detail">
+    <div class="cart_detail-group">
+        <h2>CHI TIẾT HÓA ĐƠN</h2>
+        <div class="cart_detail-info">
+            <div class="cart_detail-info-1">
+                <p>Mã Đơn Hàng: 24</p>
+                <p>Mã tài khoản: 23</p>
+                <p>Địa Chỉ: 123 abc</p>
+            </div>
+            <div class="cart_detail-info-2">
+                <p>Tên Khách Hàng: LocHoang</p>
+                <p>Số Điện Thoại: 21112233923</p>
+                <p>Ngày Đặt: 21-01-2003</p>
+            </div>
+        </div>
+        <div class="cart_detail-total">
+            <p>Tổng Tiền: <span>180.000</span>đ</p>
+        </div>
+        <div class="cart_detail-table">
+            <table class="table table_product">
+                <thead>
+                    <tr>
+                        <th scope="col">Mã Sản Phẩm</th>
+                        <th scope="col">Tên Sản Phẩm</th>
+                        <th scope="col">Size</th>
+                        <th scope="col">Số Lượng</th>
+                        <th scope="col">Giá</th>
+                        <th scope="col">Thành Tiền</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>007</th>
+                        <td>Áo đá bóng</td>
+                        <td>xl</td>
+                        <td>10</td>
+                        <td><span>180.000</span>đ</td>
+                        <td><span>1.800.000</span>đ</td>
+                    </tr>
+                </tbody>
+
+            </table>
+            
+        </div>
+        <div class="btn_submit">
+            <a href="cart.php">Xác nhận</a>
+            </div>
+    </div>
+</div>
