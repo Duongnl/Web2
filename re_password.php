3bc09b57721eb612afc44f60a8cd954a87f0e878
dinhchien1504 <91814060+dinhchien1504@users.noreply.github.com>
2024-03-23 18:59:40 +0700

    23/3

---
+++ re_password.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+  <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+
+</head>
+<body>
+    <div class="bodylogin"><section>
+        <form>
+            <h1>Repassword</h1>
+            <div class="input_login">
+                <ion-icon name="mail-outline"></ion-icon>
+                <input type ="email" placeholder='' required>
+                <label for="">User</label>
+            </div>
+            <div class="input_login">
+                <ion-icon name="lock-closed-outline"></ion-icon>
+                <input type="password" value="" required>
+                <label for="">Password</label>
+
+            </div>
+            <div class="input_login">
+                <ion-icon name="lock-closed-outline"></ion-icon>
+                <input type="password" value="" required>
+                <label for="">Re-Password</label>
+
+            </div>
+            
+            <button class="btn-login"><a href="#">Save</a></button>
+            
+        </form>
+
+    </section>
+    </div>
+</body>
+</html>
