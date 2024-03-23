a958c847cd9548f76eb2459b33798fb4528f6c65
LocHoang1 <lochoang2101@gmail.com>
2024-03-23 09:46:06 +0700

    loc responsive

--- cart.php
+++ cart.php
@@ -1,6 +1,6 @@
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="cart.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 <div class="cart_form">
     <div class="cart_form-product">
         <div class="cart_form-product-1">
