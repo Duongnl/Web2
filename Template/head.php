620af78a923beafcbb6e18220af4d0f0cdce9565
Duongg <lengocduong003@gmail.com>
2024-03-26 11:41:44 +0700

    merge product-page

--- Template/head.php
+++ Template/head.php
@@ -328,7 +328,7 @@
   
 
 </header>
-    <script type="text/javascript" src="../assets/js/bootstrap.js" ></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js" ></script>
 </body>
 </html>
 
---
+++ product-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id = "wrapper" >
+        <div id="header">
+            <?php require('template/head.php'); ?>
+        </div>
+        <div id="content">
+            <?php require('template/product.php'); ?>
+        </div>
+        <div id="footer">
+            <?php require('template/footer.php'); ?>
+        </div>
+    </div>
+    
+</body>
+</html>
