59142b9f47be489db34a90752206f7a1753fc654
dinhchien1504 <91814060+dinhchien1504@users.noreply.github.com>
2024-03-30 19:16:25 +0700

    7h16 30/3 update

---
+++ page_repass.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+       #body{
+        display: block;
+       }
+       #content {
+        margin: 5% 0px;
+        display:flex;
+        justify-content: center;
+       }
+    </style>
+</head>
+<body id="body">
+    <div id="header">
+        <?php
+        
+        include 'header.php';
+        ?>
+    </div>
+
+    <div id="content">
+        <?php
+        include 're_password.php';
+        ?>
+    </div>
+    <div id ="footer">
+        <?php
+        include 'footer.php';
+        ?>
+    </div>
+</body>
+<style>
+   
+</style>
+</html>
