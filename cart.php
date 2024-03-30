b609534415ddfa50d8a92e4ce3ea9aa4a3586845
LocHoang1 <lochoang2101@gmail.com>
2024-03-30 18:00:29 +0700

    push

--- cart.php
+++ cart.php
@@ -7,18 +7,18 @@
 <div class="cart_form">
     <div class="cart_form-product">
         <div class="cart_form-product-1">
-            <h2>Giỏ Hàng</h2>
+            <h4><a href="">Home</a> / Cart</h4>
             <div class="product_item-content">
                 <table class="table table_product">
                     <thead>
                         <tr>
-                            <th scope="col">STT</th>
-                            <th scope="col">Ảnh</th>
-                            <th scope="col">Sản Phẩm</th>
-                            <th scope="col">Đơn Giá</th>
-                            <th scope="col">Số Lượng</th>
-                            <th scope="col">Thành Tiền</th>
-                            <th scope="col">Xóa</th>
+                            <th scope="col">Order</th>
+                            <th scope="col">Img</th>
+                            <th scope="col">Name Product</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Subtotal</th>
+                            <th scope="col">Deleted</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -26,18 +26,63 @@
                             <th>1</th>
                             <td><img src="./assets/img/arsenal1.jpg" alt="" width="50px"></td>
                             <td>Áo đá bóng</td>
-                            <td><span>180.000</span>đ</td>
+                            <td><span>180</span>$</td>
                             <td><input type="number" min="0" value="1"></td>
-                            <td><span>180.000</span>đ</td>
+                            <td><span>180</span>$</td>
                             <td><button><i class="fa-solid fa-trash-can"></i></button></td>
                         </tr>
                         <tr>
                             <th>1</th>
                             <td><img src="./assets/img/arsenal1.jpg" alt="" width="50px"></td>
                             <td>Áo đá bóng</td>
-                            <td><span>180.000</span>đ</td>
+                            <td><span>180</span>$</td>
                             <td><input type="number" min="0" value="1"></td>
-                            <td><span>180.000</span>đ</td>
+                            <td><span>180</span>$</td>
+                            <td><button><i class="fa-solid fa-trash-can"></i></button></td>
+                        </tr>
+                        <tr>
+                            <th>1</th>
+                            <td><img src="./assets/img/arsenal1.jpg" alt="" width="50px"></td>
+                            <td>Áo đá bóng</td>
+                            <td><span>180</span>$</td>
+                            <td><input type="number" min="0" value="1"></td>
+                            <td><span>180</span>$</td>
+                            <td><button><i class="fa-solid fa-trash-can"></i></button></td>
+                        </tr>
+                        <tr>
+                            <th>1</th>
+                            <td><img src="./assets/img/arsenal1.jpg" alt="" width="50px"></td>
+                            <td>Áo đá bóng</td>
+                            <td><span>180</span>$</td>
+                            <td><input type="number" min="0" value="1"></td>
+                            <td><span>180</span>$</td>
+                            <td><button><i class="fa-solid fa-trash-can"></i></button></td>
+                        </tr>
+                        <tr>
+                            <th>1</th>
+                            <td><img src="./assets/img/arsenal1.jpg" alt="" width="50px"></td>
+                            <td>Áo đá bóng</td>
+                            <td><span>180</span>$</td>
+                            <td><input type="number" min="0" value="1"></td>
+                            <td><span>180</span>$</td>
+                            <td><button><i class="fa-solid fa-trash-can"></i></button></td>
+                        </tr>
+                        <tr>
+                            <th>1</th>
+                            <td><img src="./assets/img/arsenal1.jpg" alt="" width="50px"></td>
+                            <td>Áo đá bóng</td>
+                            <td><span>180</span>$</td>
+                            <td><input type="number" min="0" value="1"></td>
+                            <td><span>180</span>$</td>
+                            <td><button><i class="fa-solid fa-trash-can"></i></button></td>
+                        </tr>
+                        <tr>
+                            <th>1</th>
+                            <td><img src="./assets/img/arsenal1.jpg" alt="" width="50px"></td>
+                            <td>Áo đá bóng</td>
+                            <td><span>180</span>$</td>
+                            <td><input type="number" min="0" value="1"></td>
+                            <td><span>180</span>$</td>
                             <td><button><i class="fa-solid fa-trash-can"></i></button></td>
                         </tr>
                     </tbody>
@@ -46,40 +91,41 @@
             </div>
         </div>
         <div class="cart_form-product-2">
-            <h2>Đơn hàng</h2>
+            <h4>Cart Total</h4>
             <div class="cart_form-product-order">
                 <div class="cart_form-product-order_info">
-                    <div class="product_cost">Tổng tiền:</div>
-                    <div class="product_value">20.000</div>
+                    <div class="product_cost">Subtotal: <span>20</span></div>
+                    <div class="product_cost product_cost-ship">Shipping: <span>15</span></div>
+                    <div class="product_cost total">Total: <span>35</span></div>
                 </div>
-                <button class="btn_pay">Thanh toán</button>
+                <button class="btn_pay">Procees to checkout</button>
             </div>
         </div>
     </div>
     <div class="group_btn">
-        <button class="group_btn-item">Mua tiếp</button>
-        <button class="group_btn-item">Cập nhập giỏ hàng</button>
+        <button class="group_btn-item">Return To Shop</button>
+        <button class="group_btn-item">Update Cart</button>
     </div>
     <div class="cart_list">
         <div class="cart_list-title">
-            <h2>DANH SÁCH CÁC ĐƠN HÀNG ĐÃ ĐẶT</h2>
+            <h2>LIST OF ORDERS</h2>
         </div>
         <div class="cart_list-filler">
-            Từ <input type="date" class="input_date">
-            Đến <input type="date" class="input_date">
-            <button class="filler">Lọc</button>
+            From <input type="date" class="input_date">
+            To <input type="date" class="input_date">
+            <button class="filler">Filler</button>
             <button class="refresh">Refresh</button>
         </div>
         <div class="cart_list-item">
             <table class="table table_2">
                 <thead>
                     <tr>
-                        <th>Mã đơn</th>
-                        <th>Mã tài khoản</th>
-                        <th>Ngày đặt</th>
-                        <th>Trạng thái</th>
-                        <th>Tổng tiền</th>
-                        <th>Xem chi tiết</th>
+                        <th>Order ID</th>
+                        <th>User ID</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                        <th>Total</th>
+                        <th>View Detail</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -88,16 +134,16 @@
                         <td>data1</td>
                         <td>Data 2</td>
                         <td>Data 3</td>
-                        <td><button class="btn_status">Chưa xử lý</button></td>
+                        <td><button class="btn_status">No process</button></td>
                         <td>200.000 đ</td>
-                        <td><a href="cart_detail.php">Xem chi tiết</a></td>
+                        <td><a href="cart_detail.php">View Detail</a></td>
                     </tr>
                     <!-- Add more rows as needed -->
                 </tbody>
             </table>
         </div>
         <div class="cart_list-btn">
-            <button class="btn_see-more">Xem thêm</button>
+            <button class="btn_see-more">See More</button>
         </div>
     </div>
 </div>
--- cart_detail.php
+++ cart_detail.php
@@ -7,32 +7,32 @@
 </div>
 <div class="cart_detail">
     <div class="cart_detail-group">
-        <h2 class="cart_detail-title">CHI TIẾT HÓA ĐƠN</h2>
+        <h2 class="cart_detail-title">Cart Detail</h2>
         <div class="cart_detail-info">
             <div class="cart_detail-info-1">
-                <p>Mã Đơn Hàng: 24</p>
-                <p>Mã tài khoản: 23</p>
-                <p>Địa Chỉ: 123 abc</p>
+                <p>Order ID: 24</p>
+                <p>User ID: 23</p>
+                <p>Address: 123 abc</p>
             </div>
             <div class="cart_detail-info-2">
-                <p>Tên Khách Hàng: LocHoang</p>
-                <p>Số Điện Thoại: 21112233923</p>
-                <p>Ngày Đặt: 21-01-2003</p>
+                <p>User Name: LocHoang</p>
+                <p>Phone: 21112233923</p>
+                <p>Date: 21-01-2003</p>
             </div>
         </div>
         <div class="cart_detail-total">
-            <p>Tổng Tiền: <span>180.000</span>đ</p>
+            <p>Totail: <span>180</span>$</p>
         </div>
         <div class="cart_detail-table">
             <table class="table table_product">
                 <thead>
                     <tr>
-                        <th scope="col">Mã Sản Phẩm</th>
-                        <th scope="col">Tên Sản Phẩm</th>
+                        <th scope="col">Product ID</th>
+                        <th scope="col">Product Name</th>
                         <th scope="col">Size</th>
-                        <th scope="col">Số Lượng</th>
-                        <th scope="col">Giá</th>
-                        <th scope="col">Thành Tiền</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Cost</th>
+                        <th scope="col">Totail</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -41,8 +41,8 @@
                         <td>Áo đá bóng</td>
                         <td>xl</td>
                         <td>10</td>
-                        <td><span>180.000</span>đ</td>
-                        <td><span>1.800.000</span>đ</td>
+                        <td><span>180</span>$</td>
+                        <td><span>1.800</span>$</td>
                     </tr>
                 </tbody>
 
@@ -50,7 +50,7 @@
             
         </div>
         <div class="btn_submit">
-            <a href="cart.php" class ="btn_submit-a">Xác nhận</a>
+            <a href="cart.php" class ="btn_submit-a">Confirm</a>
             </div>
     </div>
 </div>
