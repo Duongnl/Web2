a7e701c7586d65b198fd77ad0183586bccdce6f5
Duongg <lengocduong003@gmail.com>
2024-04-02 17:11:51 +0700

    delete

--- site/view/product-detail-page.php
+++ site/view/product-detail-page.php
@@ -4,18 +4,194 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="../../public/css/product-detail.css">
+    <style>
+    
+      </style>
 </head>
 <body>
-    <div id = "wrapper" >
-        <div id="header">
-            <?php require('../../public/template/site/head.php'); ?>
+<div class="chiTietMonHang_main">
+    <div class="container">
+        <div class="row chiTietMonHang_SanPham">
+           <div class="row col-sm-12 col-md-8 chiTietMonHang_SanPham_left mb-sm-4">
+            <div class="chiTietMonHang_chiTietCacAnh  col-md-2 col-sm-12 ">
+              <div class="row chiTietMonHang_chiTietCacAnh_Phu" >
+                <div class="col-3 col-md-12 chiTietMonHang_chiTietCacAnh_Anh p-0 ">
+                  <img src="../../public/images/img.png" class="img-fluid img_cacAnh" alt="...">
+                </div>
+                <div class="col-3 col-md-12 chiTietMonHang_chiTietCacAnh_Anh p-0">
+                  <img src="../../public/images/img.png" class="img-fluid img_cacAnh" alt="...">
+                </div>
+                <div class="col-3 col-md-12 chiTietMonHang_chiTietCacAnh_Anh p-0">
+                  <img src="../../public/images/img.png" class="img-fluid img_cacAnh" alt="...">
+                </div>
+                <div class="col-3 col-md-12 chiTietMonHang_chiTietCacAnh_Anh p-0" >
+                  <img src="../../public/images/img.png" class="img-fluid img_cacAnh" alt="...">
+                </div>
+              </div>
+             
+          </div>
+          <div class="chiTietMonHang_AnhChinh  col-md-5 col-sm-12 chiTietMonHang_SanPham_right">
+              <img src="../../public/images/img.png" class="img-fluid img_anhChinh" alt="...">
+          </div>
+           </div>
+           <!-- thông tin -->
+            <div class="chiTietMonHang_thongTin col-sm-12  col-md-4">
+             
+                <h1 class="chiTietMonHang_title pb">Havic HV G-92 Gamepad</h1>    
+                <div class="chiTietMonHang_AnhChinh_price">$192.00</div>
+                <p class="product-description">
+                ubble free install &amp; mess free removal Pressure sensitive.
+                </p>
+                <hr class="separator" />
+                <div class="huongDanChonSize">
+                    <a href="../../public/images/img.png">*Xem hướng dẫn chọn size</a>
+                </div>
+                <div class="chiTietMonHang_Size chonSize row">
+                  <div class="col-3 tag_size">Size:</div>
+                  <form class="col-9">
+                    <input type="radio" id="sizeM" name="size" value="sizeM" class="size" checked>
+                    <label for="sizeM" class="label_Size sizeM">M</label>
+                    <input type="radio" id="sizeL" name="size" value="sizeL" class="size">
+                    <label for="sizeL" class="label_Size sizeL">L</label>
+                    <input type="radio" id="sizeS" name="size" value="sizeS" class="size">
+                    <label for="sizeS" class="label_Size sizeS">S</label>
+                  </form>
+                 
+                </div>
+                
+                
+                <div class="chiTietMonHang_soLuong row mt-3 justify-content-center">
+                    <div class="size-label col-xxl-3 col-3">Số lượng:</div>
+
+                    <div class="chiTietMonHang_thongTin_BUY_SoLuong col-xxl-9 col-9">
+                        <button id="decrease" class="btn_giamSoLuong">-</button>
+                        <input type="text" id="chiTietMonHang_thongTin_BUY_SoLuong" class="chiTietMonHang_thongTin_BUY_SoLuong" value="1" readonly>
+                        <button id="increase" class="btn_tangSoLuong">+</button>
+                    </div>
+                    <div class=" col-12 chiTietMonHang_themVaoGioHang mt-4">
+                      <button class="btn_ThemVaoGioHang">Thêm vào giỏ hàng</button>
+                    </div>
+                </div>
+                <div class="delivery-info">
+                  <div class="free-delivery">
+                    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/2a23f35277ec287bdea2870638bbdacf171666c11a657aa3e10b9901a2334e8b?apiKey=de754edacf6d4fbeaf990b709fcfe0b5&" alt="Free delivery icon" class="delivery-icon" />
+                    <div class="delivery-details">
+                      <div class="delivery-label">Free Delivery</div>
+              <div class="delivery-input">
+                        Enter your postal code for Delivery 
+                      </div>
+                    </div>
+                  </div>
+                  <hr class="delivery-separator" />
+                  <div class="return-policy">
+                    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/e8eb9d4232c5c4b5fb4e8c5b395d852ff88717160dc1ebc4089000daaa178e67?apiKey=de754edacf6d4fbeaf990b709fcfe0b5&" alt="Return policy icon" class="return-icon" />
+                    <div class="return-details">
+                      <div class="return-label">Return Delivery</div>
+                      <div class="return-description">
+                        Free 30 Days Delivery Returns. 
+                      </div>
+                    </div>
+                  </div>
+            </div>
+        
+           </div>
         </div>
-        <div id="content">
-            <?php require('../../public/template/site/product-detail.php'); ?>
+        <!-- sản phẩm liên quan -->
+        <div class="row">
+          <div class=" sanPhamLienQuan">
+            <div class="row sanPhamLienQuan_top">
+              <p class="cacSanPhamKhac col-2">
+              </p>
+              <p class="col-10 sanPhamLienQuan_top_text">CAC SAN PHAM</p>
+            </div>
+            
+            <div class="row sanPhamLienQuan_bottom">
+              <div class="col-6  col-md-3 col-xxl-3 product">
+                <a href="" class="wrap-img">
+                <img class="img-product" src="../../public/images/img.png">
+                <div class="deal"><span>-50%</span></div>
+                </a>
+                <div class="d-flex justify-content-between align-items-center mx-1 my-2">
+                    <div class="product-body">
+                        <a href="../../public/images/img.png" class="product-title">The north coat</a>
+                        <div class="prices">
+                        <div class="new-price">$260</div>
+                        <div class="old-price">$260</div>
+                        </div>
+                    </div>
+                    <button type="button" class="btn btn-outline-primary btn-add-to-cart">
+                        <i class="fa-solid fa-cart-plus"></i>
+                    </button>
+                </div>
+              </div>
+            <div class="col-6  col-md-3 col-xxl-3 product">
+              <a href="" class="wrap-img">
+              <img class="img-product" src="../../public/images/img.png">
+              <div class="deal"><span>-50%</span></div>
+              </a>
+              <div class="d-flex justify-content-between align-items-center mx-1 my-2">
+                  <div class="product-body">
+                      <a href="../../public/images/img.png" class="product-title">The north coat</a>
+                      <div class="prices">
+                      <div class="new-price">$260</div>
+                      <div class="old-price">$260</div>
+                      </div>
+                  </div>
+                  <button type="button" class="btn btn-outline-primary btn-add-to-cart">
+                      <i class="fa-solid fa-cart-plus"></i>
+                  </button>
+              </div>
+            </div>
+            <div class="col-6  col-md-3 col-xxl-3 product">
+            <a href="" class="wrap-img">
+            <img class="img-product" src="../../public/images/img.png">
+            <div class="deal"><span>-50%</span></div>
+            </a>
+            <div class="d-flex justify-content-between align-items-center mx-1 my-2">
+                <div class="product-body">
+                    <a href="../../public/images/img.png" class="product-title">The north coat</a>
+                    <div class="prices">
+                    <div class="new-price">$260</div>
+                    <div class="old-price">$260</div>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-outline-primary btn-add-to-cart">
+                    <i class="fa-solid fa-cart-plus"></i>
+                </button>
+            </div>
+             </div>
+             <div class="col-6  col-md-3 col-xxl-3 product">
+          <a href="" class="wrap-img">
+          <img class="img-product" src="../../public/images/img.png">
+          <div class="deal"><span>-50%</span></div>
+          </a>
+          <div class="d-flex justify-content-between align-items-center mx-1 my-2">
+              <div class="product-body">
+                  <a href="../../public/images/img.png" class="product-title">The north coat</a>
+                  <div class="prices">
+                  <div class="new-price">$260</div>
+                  <div class="old-price">$260</div>
+                  </div>
+              </div>
+              <button type="button" class="btn btn-outline-primary btn-add-to-cart">
+                  <i class="fa-solid fa-cart-plus"></i>
+              </button>
+          </div>
+             </div>
+            </div>
+          </div>
         </div>
-        <div id="footer">
-            <?php require('../../public/template/site/footer.php'); ?>
-        </div>
-    </div>
+        
+        
+
+      </div>
+</div>
+</div>
+
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
+<script src="../../public/js/product-detail.js"></script>
 </body>
 </html>
