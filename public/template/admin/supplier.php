a3a2ba13388daa010327a9a7c8fb35fa4ff5c351
Duongg <lengocduong003@gmail.com>
2024-04-09 16:35:09 +0700

    admin

---
+++ public/template/admin/supplier.php
@@ -0,0 +1,43 @@
+
+<div class="main-content">
+    <h3 class="h1-head-name" >Supplier management</h3>
+    
+    <table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">STT</th>
+      <th scope="col">Supplier ID</th>
+      <th scope="col">Supplier name</th>
+      <th scope="col">Act</th>
+      
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+</div>
