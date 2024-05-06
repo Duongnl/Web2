e0b67ae91897697eb7ff8e8aea0c97c9d08a9910
trancuong19022 <113131294+trancuong19022@users.noreply.github.com>
2024-05-06 22:42:16 +0700

    Update quanLyDonHang_controller.php

--- admin/controller/quanLyDonHang_controller.php
+++ admin/controller/quanLyDonHang_controller.php
@@ -1,5 +1,6 @@
 <?php session_start();
 require_once('../model/quanLyDonHang_model.php');
+require_once('../model/chiTietDonHang_model.php');
 require_once('../model/db_config.php');
 if ( isset($_POST['action'])  && isset($_POST['MaHD']) )
 {
@@ -7,12 +8,22 @@ if ( isset($_POST['action'])  && isset($_POST['MaHD']) )
    $MaHD = $_POST['MaHD'];
    $quanLyDonHang_Model = new quanLyDonHang_model();
    if ($action == 'XuLy') {
-        $quanLyDonHang_Model->UpdateTrangThaiDonHang( trim($MaHD),1);
+        $ctdn_md= new chiTietDonHang_model();
+        if($ctdn_md->updateSoLuong_updateTrangThai(trim($MaHD))){
+            $quanLyDonHang_Model->UpdateTrangThaiDonHang(trim($MaHD),1);
+            $_SESSION['Duyet_ThanhCong'] = true;
+            $_SESSION['Duyet_ThatBai'] = false;
+            
+        }else{
+         
+            $_SESSION['Duyet_ThatBai'] = true;
+            $_SESSION['Duyet_ThanhCong'] = false;
+        }
     }
     else if ($action =='Huy') {
         $quanLyDonHang_Model->UpdateTrangThaiDonHang( trim($MaHD),2);
     }
-    $_SESSION['quanLyDonHang_controller'] = true;
+
     header("Location: ../view/quanLyDonHang_page.php");
     exit; // Dừng thực thi PHP sau khi chuyển hướng
 
